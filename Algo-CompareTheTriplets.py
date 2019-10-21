821beb36a2919e5bf8fd246450fd364ada8b9a95
sainuka <sainuka@gmail.com>
2019-10-21 07:57:27 -0500

    Create Algo-CompareTheTriplets.py

---
+++ Algo-CompareTheTriplets.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def compareTriplets(a, b):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = map(int, raw_input().rstrip().split())
+
+    b = map(int, raw_input().rstrip().split())
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

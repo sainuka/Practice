2a80f57aea0c0ddf47ea37bdbf568242746a84dd
sainuka <sainuka@gmail.com>
2019-10-21 10:38:06 -0500

    Create Algo-MiniMaxSum.py

---
+++ Algo-MiniMaxSum.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def miniMaxSum(arr):
+
+if __name__ == '__main__':
+    arr = map(int, raw_input().rstrip().split())
+
+    miniMaxSum(arr)

d4cca52a5d3375aed4d424c855232b9848aa5689
sainuka <sainuka@gmail.com>
2019-10-21 10:33:56 -0500

    Create Algo-PlusMinus.py

---
+++ Algo-PlusMinus.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def plusMinus(arr):
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    arr = map(int, raw_input().rstrip().split())
+
+    plusMinus(arr)

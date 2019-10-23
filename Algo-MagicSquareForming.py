bd1b4875062859b7cad9563cc2816424e0bc5587
sainuka <sainuka@gmail.com>
2019-10-22 20:20:33 -0500

    Create Algo-MagicSquareForming.py

---
+++ Algo-MagicSquareForming.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def formingMagicSquare(s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = []
+
+    for _ in xrange(3):
+        s.append(map(int, raw_input().rstrip().split()))
+
+    result = formingMagicSquare(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

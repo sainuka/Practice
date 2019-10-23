a44dd8d63214059713190b82aea588721350a389
sainuka <sainuka@gmail.com>
2019-10-22 20:09:01 -0500

    Create Algo-CamelCase.py

---
+++ Algo-CamelCase.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def camelcase(s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = raw_input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

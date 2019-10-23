87708c55edfb4473ac231013f02a4b904ba50cd6
sainuka <sainuka@gmail.com>
2019-10-22 20:11:44 -0500

    Create Algo-StrongPassword.py

---
+++ Algo-StrongPassword.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+def minimumNumber(n, password):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(raw_input())
+
+    password = raw_input()
+
+    answer = minimumNumber(n, password)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()

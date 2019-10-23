d28e5fde3f0eac5ea0c8ede0826639e666f23ee4
sainuka <sainuka@gmail.com>
2019-10-23 13:16:27 -0500

    Update JavaRegex2.java

--- JavaRegex2.java
+++ JavaRegex2.java
@@ -5,28 +5,25 @@ import java.util.regex.Pattern;
 public class DuplicateWords {
 
     public static void main(String[] args) {
-
-        String regex = "/* Write a RegEx matching repeated words here. */";
-        Pattern p = Pattern.compile(regex, /* Insert the correct Pattern flag here.*/);
+        String regex = "\\b(\\w+)(\\W+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
         Scanner in = new Scanner(System.in);
         int numSentences = Integer.parseInt(in.nextLine());
-        
+
         while (numSentences-- > 0) {
             String input = in.nextLine();
-            
+
             Matcher m = p.matcher(input);
-            
+
             // Check for subsequences of input that match the compiled pattern
             while (m.find()) {
-                input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
+                input = input.replaceAll(m.group(), m.group(1));
             }
-            
+
             // Prints the modified sentence.
             System.out.println(input);
         }
-        
         in.close();
     }
 }
-
